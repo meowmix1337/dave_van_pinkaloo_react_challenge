9c8dcb1153592e9f5e8c5de1b7a06d185b00e5b7
meowmix1337 <57775817+meowmix1337@users.noreply.github.com>
2019-12-12 11:02:03 -0500

    Username updates

--- src/components/UserBalance.js
+++ src/components/UserBalance.js
@@ -9,17 +9,19 @@ export default ({ user, users }) => {
 
 	const userData = findUser(user.id)
 
+	//duplicate code, can move out to a util function
 	let userName = "Anonymous"
     let image = null
 
 	if(userData) {
-		userName = userData && userData.first_name && userData.last_name
+		userName = userData.first_name && userData.last_name ? 
+		  userData.first_name + " " + userData.last_name : userData.first_name
 		image = userData.image
-	  }
+	}
 
 	return <div className="UserBalance">
 		<img className="UserImage ProfileImage" alt="" src={ image } />
-		<strong className="ProfileName">{ userName }</strong>
+		<strong className="ProfileName">Welcome, { userName }</strong>
 		<div className="UserBalance-amount">{ Numeral(user.balance).format('$0,0.00') }</div>
 		<label>Available</label>
 	</div>
