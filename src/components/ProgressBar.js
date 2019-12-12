0a06826929585f3a16a50134e0073ad7551199f8
meowmix1337 <57775817+meowmix1337@users.noreply.github.com>
2019-12-12 10:57:09 -0500

    removing log

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -4,7 +4,6 @@ import Numeral from 'numeral'
 
 function ProgressBar({ progress }) {
   const width = Numeral(progress).format('0%')
-  console.log(progress)
   return <div className="ProgressBar">
       <div className={ (progress >= 1.0 ? "ProgressBar-complete": "ProgressBar-progress") } style={{ width }}>
         
